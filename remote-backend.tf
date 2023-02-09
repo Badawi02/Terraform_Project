b70ad0c6e34d379d86a7c8f22df2e0a7b304966a
Ahmed Badawi <96023421+Badawi02@users.noreply.github.com>
2023-02-09 03:51:34 +0200

    Update remote-backend.tf

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,7 +1,7 @@
 
  terraform {
    backend "s3" {
-     bucket         = "badawi-terraform-state-backend"
+     bucket         = "badawi-s3-state-lock"
      key            = "terraform.tfstate"
      region         = "us-east-1"
      dynamodb_table = "terraform_state"
